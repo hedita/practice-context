ca9df101f2ef9234b3e7d9392932cad6f4222154
Hedita <hedi.takrami@gmail.com>
2023-03-18 12:56:18 +0330

    add App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import ReactDOM from "react-dom/client";
+import SearchInput from "./SearchInput";
 
 const App = () => {
-    return "hi"
+    return <SearchInput />
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
